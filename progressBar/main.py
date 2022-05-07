877c904ff4822038a8397f4eaf25e920e73db689
⸸ ジグソー ⛧ <102450738+j1gs4w-exe@users.noreply.github.com>
2022-05-07 06:16:13 +0000

    Add files via upload

---
+++ progressBar/main.py
@@ -0,0 +1,16 @@
+import math
+
+
+
+def progress_bar(progress, total):
+    percent = 100 * (progress / float(total))
+    bar = '*' * int(percent) + '-' * (100 - int(percent))
+    print(f"\r|{bar}| {percent:.2f}%", end="\r")
+
+numbers = [x * 5 for x in range(2000, 3000)]
+results = []
+
+progress_bar(0, len(numbers))
+for i, x in enumerate(numbers):
+    results.append(math.factorial(x))
+    progress_bar(i + 1, len(numbers))
