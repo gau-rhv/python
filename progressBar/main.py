3796e3e670be2b24482b3bc02468b38f00a59e4f
⸸ ジグソー ⛧ <102450738+j1gs4w-exe@users.noreply.github.com>
2022-05-08 04:04:46 +0000

    Update main.py

--- progressBar/main.py
+++ progressBar/main.py
@@ -4,7 +4,7 @@ import math
 
 def progress_bar(progress, total):
     percent = 100 * (progress / float(total))
-    bar = '*' * int(percent) + '-' * (100 - int(percent))
+    bar = '#' * int(percent) + '-' * (100 - int(percent))
     print(f"\r|{bar}| {percent:.2f}%", end="\r")
 
 numbers = [x * 5 for x in range(2000, 3000)]
