9304c588eec7b5f7a89ad20a0dd848c5b9c5dce0
⸸ ジグソー ⛧ <102450738+j1gs4w-exe@users.noreply.github.com>
2022-06-24 04:55:28 +0530

    Update prgm3.py

--- school/prgm3.py
+++ school/prgm3.py
@@ -20,7 +20,7 @@ while True:
     opt = int(input(">> Enter your Option: "))
     
     if opt == 1:
-        l = list(eval(input(">> Enter List {Need Even no.of Elen: ")))
+        l = list(eval(input(">> Enter List {Need Even noof Elen: ")))
         print("Before Swap: ",l)
         L = swapHalf(l)
         print("After Swap: ",L)
