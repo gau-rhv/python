2986010c3b24be966c8b36d8d088e0a80305b15b
⸸ ジグソー ⛧ <102450738+j1gs4w-exe@users.noreply.github.com>
2022-07-21 04:15:27 +0530

    Update p1.py

--- school/p1.py
+++ school/p1.py
@@ -1,10 +1,18 @@
 def np(n):
   r = c = s =0
   while n!=0:
-    l = n%10
-    r = r * 10 + l
+    l = n % 10
     c += 1
     s += l
+    r = r * 10 + l
     n = n // 10
    return r, c, s
+    
+def arm(n, c):
+  t = n
+  c = 0
+  while n!=0:
+    l = t % 10
+    s = s ** l + c
+    t = t // 10
     
